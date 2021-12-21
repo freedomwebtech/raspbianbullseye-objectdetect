04cbc382ef5086c909c6f591e704666f84e1a8fb
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2021-12-21 12:17:02 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+sudo pip3 install opencv-python 
+sudo pip3 install gdown
+gdown https://drive.google.com/uc?id=1BLXP7RKEfTp9fxbmI8Qu2FdhU7NUxcwV
+sudo -H pip3 install tensorflow-2.2.0-cp37-cp37m-linux_armv7l.whl wrapt --upgrade --ignore-installed
+sudo pip3 install --upgrade numpy
+sudo pip3 install cvlib
+sudo pip3 install keras
+sudo apt-get install libatlas-base-dev -y
