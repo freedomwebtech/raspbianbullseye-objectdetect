dd0c5bb48ef30e5a4700e179115a3e9e1f6532e4
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2021-12-22 14:05:30 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,9 @@ sudo apt install libgtk2.0-dev pkg-config cmake pkg-config libhdf5-dev libgtk2.0
 
 sudo apt-get install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev -y
 
-sudo pip3 install opencv-python 
+sudo pip3.7 install opencv-python 
 sudo pip3 install --upgrade numpy
-sudo pip3 install cvlib
-sudo pip3 install keras
+sudo pip3.7 requests progressbar pillow
+sudo pip3.7 install cvlib
+sudo pip3.7 install keras
 sudo apt-get install libatlas-base-dev -y
