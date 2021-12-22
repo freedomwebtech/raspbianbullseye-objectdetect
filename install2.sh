1dcbd9a33608c9c7f13065b5dfffca95dcd15156
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2021-12-22 14:10:23 +0530

    Create install2.sh

---
+++ install2.sh
@@ -0,0 +1,6 @@
+sudo pip3.7 install opencv-python 
+sudo pip3.7 install --upgrade numpy
+sudo pip3.7 requests progressbar pillow
+sudo pip3.7 install cvlib
+sudo pip3.7 install keras
+sudo apt-get install libatlas-base-dev -y
