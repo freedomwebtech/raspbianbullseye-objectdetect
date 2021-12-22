ab3553c365767f67bf36a6a1cf51192a2f47936d
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2021-12-22 14:14:20 +0530

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,6 +1,7 @@
 sudo pip3.7 install opencv-python 
 sudo pip3.7 install --upgrade numpy
 sudo pip3.7 requests progressbar pillow
+sudo pip3.7 install tensorflow-2.5.1-cp37-cp37m-linux_aarch64.whl
 sudo pip3.7 install cvlib
 sudo pip3.7 install keras
 sudo apt-get install libatlas-base-dev -y
