14e7f0a463af06c77a2a182a7137c69773d30f49
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2021-12-24 19:03:18 +0530

    Add files via upload

---
+++ install2.sh
@@ -0,0 +1,6 @@
+sudo pip3 install opencv-python==4.5.3.56 
+sudo pip3 install -U numpy
+sudo pip3 install tensorflow-2.6.0-cp39-cp39-linux_aarch64.whl
+sudo pip3 install cvlib
+sudo pip3 install keras
+sudo apt-get install libatlas-base-dev -y
