70e227cd6abca10eae1dcbb4ac2a90b22b91d2f8
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2021-12-24 19:02:24 +0530

    Delete install2.sh

--- install2.sh
+++
@@ -1,6 +0,0 @@
-sudo pip3 install opencv-python 
-sudo pip3 install --upgrade numpy
-sudo pip3 install tensorflow-2.6.0-cp39-cp39-linux_aarch64.whl
-sudo pip3 install cvlib
-sudo pip3 install keras
-sudo apt-get install libatlas-base-dev -y
