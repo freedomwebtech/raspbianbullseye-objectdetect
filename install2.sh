30b23f72e0cfcaf2d19628827baf530b37951532
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2021-12-25 07:37:47 +0530

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,5 +1,5 @@
 sudo pip3 install opencv-python==4.5.3.56 
-sudo pip3 install -U numpy
+pip install numpy==1.19.2
 sudo pip3 install tensorflow-2.6.0-cp39-cp39-linux_aarch64.whl
 sudo pip3 install cvlib
 sudo pip3 install keras
